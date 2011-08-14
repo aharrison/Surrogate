1a82389ef5684e3edbfaa27b348598b80827bb85
Andrew Harrison <atharris@calpoly.edu>
2011-08-13 23:14:36 -0700

    Fixed gallery cover images

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ActiveRecord::Base
   has_many :images
   belongs_to :cover_image, :class_name => 'Image', :foreign_key => 'cover_image_id'
+
+  def cover_thumbnail_path
+    if cover_image then "/images/#{cover_image.id}_thumbnail.jpg" end
+  end
 end
